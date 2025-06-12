eb04627a15cf0334909c10d6783218b0423466dd
LocalNews9908 <sudhavanamala6@gmail.com>
2025-06-12 06:18:39 +0530

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,11 +1,11 @@
-
 const firebaseConfig = {
   apiKey: "AIzaSyBcccMSnuCrJutyV4hhEzy76JjS94BZgbA",
   authDomain: "localnews-a22fe.firebaseapp.com",
   projectId: "localnews-a22fe",
-  storageBucket: "localnews-a22fe.firebasestorage.app",
+  storageBucket: "localnews-a22fe.appspot.com",
   messagingSenderId: "831268084466",
   appId: "1:831268084466:web:6556bab4d5691513c8ce6b",
-  measurementId: "G-DY44FE58DZ"
+  measurementId: "G-DY44FE58DZ",
+  databaseURL: "https://localnews-a22fe-default-rtdb.firebaseio.com/"
 };
 firebase.initializeApp(firebaseConfig);
--- script.js
+++ script.js
@@ -1,55 +1,14 @@
+const db = firebase.database();
 
-// Your Firebase config here
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT_ID.firebaseio.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
-firebase.initializeApp(firebaseConfig);
-
-if (document.getElementById("newsForm")) {
-  document.getElementById("newsForm").addEventListener("submit", function(e) {
-    e.preventDefault();
-    const title = document.getElementById("title").value;
-    const content = document.getElementById("content").value;
-    const district = document.getElementById("district").value;
-
-    firebase.database().ref("posts").push({
-      title,
-      content,
-      district
-    }).then(() => {
-      alert("News posted!");
-      document.getElementById("newsForm").reset();
-    });
-  });
-}
-
-function filterByDistrict(districtName) {
-  const container = document.getElementById("newsContainer");
-  container.innerHTML = "Loading news for " + districtName + "...";
-
-  firebase.database().ref("posts").orderByChild("district").equalTo(districtName).once("value", function(snapshot) {
-    container.innerHTML = "";
-    if (!snapshot.exists()) {
-      container.innerHTML = "No news found for " + districtName;
-      return;
-    }
-
-    snapshot.forEach(function(childSnapshot) {
-      const post = childSnapshot.val();
-      const div = document.createElement("div");
-      div.innerHTML = `
-        <h3>${post.title}</h3>
-        <p>${post.content}</p>
-        <small>${post.district}</small>
-        <hr>
-      `;
-      container.appendChild(div);
+function filterByDistrict(district) {
+  const newsContainer = document.getElementById('news-container');
+  newsContainer.innerHTML = '';
+  db.ref('news').orderByChild('district').equalTo(district).once('value', snapshot => {
+    snapshot.forEach(child => {
+      const data = child.val();
+      const div = document.createElement('div');
+      div.innerHTML = `<h3>${data.title}</h3><p>${data.content}</p>`;
+      newsContainer.appendChild(div);
     });
   });
 }
